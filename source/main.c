4503d3ea1874a63c0b9915b977f266ce50cc18f6
Biqian Cheng <bchen158@ucr.edu>
2021-01-18 00:26:43 -0800

    Initialize repo

---
+++ source/main.c
@@ -0,0 +1,41 @@
+/*	Author: Biqian Cheng
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab 3  Exercise 5
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void)
+{
+    /* Insert DDR and PORT initializations */
+    DDRB=0xFE;PORTB=0x01;
+    DDRD=0x00;PORTD=0xFF;
+    unsigned char d=0x00;
+    unsigned char b0=0x00;
+    unsigned char b=0x00;
+    unsigned short w=0;
+    while (1) {
+	d=PIND;
+	w=d*2;
+	b=PINB;
+	b0=b&0x01;
+	w=w+b0;
+	if(w>=70){
+		PORTB=(b&0x01)|0x02;
+	}
+	else if((w>5) &&(w<70)){
+		PORTB=(b&0x01)|0x04;
+	}
+	else{
+		PORTB=b&0x01;
+	}	
+	}
+	return 1;
+}
